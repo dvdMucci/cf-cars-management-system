c86dd052ac7c5ccf6efb9b7de7d5fae599a7f91a
root <root@Docker.10.0.0.1>
2024-10-20 01:42:24 +0000

    Add primero.sh

--- primero.sh
+++ primero.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/
-bash -c "$(wget -qLO - https://github.com/dvdMucci/cf-cars-management-system/raw/main/primero.sh)"
+git clone https://github.com/dvdMucci/cf-cars-management-system/
 cd cf-cars-management-system
 sh automation.sh
