64426cf3d8f58a7ffb044baf0b16aaf8cf1c5e44
root <root@Docker.10.0.0.1>
2024-10-20 02:03:04 +0000

    Se agrega explicacion a primero.sh

--- primero.sh
+++ primero.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "En 10 segundos este sctript clona el repositorio https://github.com/dvdMucci/cf-cars-management-system/"
+echo "y ejetuta lo neseario para dejar corriendo un contenedor de docker con una aplicacion web"
+echo 'Si desea cancelar precione las teclas "ctrl" y "c"'
+sleep 10 
 cd ~/
 git clone https://github.com/dvdMucci/cf-cars-management-system/
 cd cf-cars-management-system
