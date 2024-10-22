f247cf3b58a123ed2f8913edab7be26976b74dd2
root <root@Docker.10.0.0.1>
2024-10-22 03:23:46 +0000

    Se mejora consola

--- primero.sh
+++ primero.sh
@@ -6,29 +6,33 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 print_message() {
-    local color=$1
-    local message=$2
-    echo -e "${color}${message}${NC}"
+    local flag=$1
+    local color=$2
+    local message=$3
+    echo $flag "${color}${message}${NC}"
 }
-print_message $YELLOW "En 15 segundos este sctript clona el repositorio https://github.com/dvdMucci/cf-cars-management-system/\ny ejetuta lo neseario para dejar corriendo un contenedor de docker con una aplicacion web"
-print_message $YELLOW 'Si desea cancelar precione las teclas "ctrl" y "c"'
+print_message -e $YELLOW "En 15 segundos este sctript clona el repositorio https://github.com/dvdMucci/cf-cars-management-system/\ny ejetuta lo neseario para dejar corriendo un contenedor de docker con una aplicacion web"
+print_message -e $YELLOW 'Si desea cancelar precione las teclas "ctrl" y "c"'
 
 countdown() {
     for (( i=15; i>=0; i-- ))
     do
         if [ $i -le 4 ]; then
-            print_message "$RED" "Quedan $i segundos"
+            print_message -ne "$RED" "                         \r"
+            print_message -ne "$RED" "Quedan $i segundos\r"            
         elif [ $i -le 15 ] && [ $i -gt 4 ]; then
-            print_message "$YELLOW" "Quedan $i segundos"
+            print_message -ne "$RED" "                         \r"
+            print_message -ne "$YELLOW" "Quedan $i segundos\r"
         fi
         sleep 1
     done
+    print_message -e "$RED" ""
 }
 countdown
 
-print_message $YELLOW "Se clona repositorio"
+print_message -e $YELLOW "Se clona repositorio"
 cd ~/
 git clone https://github.com/dvdMucci/cf-cars-management-system/
 cd cf-cars-management-system
-print_message $YELLOW "Se ejecuta automatizacion de tareas"
+print_message -e $YELLOW "Se ejecuta automatizacion de tareas"
 sh automation.sh
